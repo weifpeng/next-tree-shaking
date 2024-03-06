bcaa15697034621694291d0605e70f036aec10b6
luke <weifeng1994213@hotmail.com>
2024-03-06 22:39:59 +0800

    commit

--- apps/test/src/_middleware.ts
+++
@@ -1,7 +0,0 @@
-export { default } from "next-auth/middleware";
- 
-export const config = {
-  matcher: [
-    "/((?!_next/static|_next/image|favicon.ico).*)",
-  ],
-};
--- apps/test/src/pages/index.tsx
+++ apps/test/src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push("/app");
+    router.push("/a");
   }, []);
 
   return (
