86c3eff3bbde59463cf9316def4d3933cac4eba7
luke <weifeng1994213@hotmail.com>
2024-03-06 23:02:15 +0800

    add readme

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@repo/ui"],
-  // experimental: {
-  //   optimizePackageImports: [
-  //    '@repo/ui'
-  //   ],
-  // },
+  experimental: {
+    optimizePackageImports: [
+     '@repo/ui'
+    ],
+  },
 };
 
 
