3f61a3bb04d5920e92039dd5d5d1305918d95fc4
luke <weifeng1994213@hotmail.com>
2024-03-06 22:57:09 +0800

    commit

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@repo/ui"],
-  experimental: {
-    optimizePackageImports: [
-     '@repo/ui'
-    ],
-  },
-  output: "standalone",
+  // experimental: {
+  //   optimizePackageImports: [
+  //    '@repo/ui'
+  //   ],
+  // },
 };
 
 
