646dd154306518298b44268aa285184b5a1cb2d7
luke <weifeng1994213@hotmail.com>
2024-03-06 22:37:37 +0800

    commit

--- apps/test/next.config.js
+++ apps/test/next.config.js
@@ -1,48 +1,12 @@
-const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
-  // transpilePackages: ["@life/openai", "@life/ui", "@life/apps-chatgptweb-common"],
+  transpilePackages: ["@repo/ui"],
   experimental: {
-    optimizePackageImports: [
-      '@repo/ui',
-     
-    ],
-    outputFileTracingRoot: path.join(__dirname, "../../"),
-    externalDir: true,
-    serverActions: true,
-    serverActionsBodySizeLimit: '10mb',
+    // optimizePackageImports: [
+    //   // '@repo/ui',
+    // ],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: [
-        {
-          loader: "@svgr/webpack",
-          options: { svgo: false },
-        },
-      ],
-    });
-    config.experiments = config.experiments || {};
-    config.experiments.topLevelAwait = true;
-    config.experiments.asyncWebAssembly = true
-
-    return config;
-  },
-  output: "standalone",
-  async redirects() {
-    return [
-      {
-        source: "/chat-gpt",
-        destination: "/app/chat",
-        permanent: true,
-      }
-    ]
-
-  }
 };
 
 
